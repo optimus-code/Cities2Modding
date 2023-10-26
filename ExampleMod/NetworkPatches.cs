1c1565a1f520c769061879e5f3694b17968da5f7
Dan <12039578+optimus-code@users.noreply.github.com>
2023-10-26 08:04:23 +0100

    Add a radio mod, tree mod and mod to show units instead of meters for roads. (Must use Metric inorder for calculation to work currently)

---
+++ ExampleMod/NetworkPatches.cs
@@ -0,0 +1,25 @@
+﻿using Game.UI.Localization;
+using Game.UI.Tooltip;
+using HarmonyLib;
+
+namespace ExampleMod
+{
+    /// <summary>
+    /// Change length display to Units like Cities Skylines 1
+    /// </summary>
+    [HarmonyPatch( typeof( NetCourseTooltipSystem ), "OnUpdate" )]
+    class NetCourseTooltipSystem_OnUpdatePatch
+    {
+        static void Postfix( NetCourseTooltipSystem __instance )
+        {
+            var m_Length = Traverse.Create( __instance ).Field( "m_Length" ).GetValue<FloatTooltip>( );
+
+            if ( m_Length != null )
+            {
+                m_Length.value /= 8f; // Convert to Cities 1 units
+                m_Length.unit = "floatTwoFractions"; // Change to a generic unit type to stop showing m/ft
+                m_Length.label = LocalizedString.Value( "U" ); // Adjust the label to say 'U'
+            }
+        }
+    }
+}
---
+++ ExampleMod/RadioPatches.cs
@@ -0,0 +1,43 @@
+﻿using Game.Audio.Radio;
+using HarmonyLib;
+using static Game.Audio.Radio.Radio;
+
+namespace ExampleMod
+{
+    //
+    // Disables all radio clips other than Playlist items (Music related)
+    //
+
+    [HarmonyPatch( typeof( Radio ), "QueueEmergencyClips" )]
+    class Radio_QueueEmergencyClipsPatch
+    {
+        static bool Prefix( )
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch( typeof( Radio ), "QueueEmergencyIntroClip" )]
+    class Radio_QueueEmergencyIntroClipPatch
+    {
+        static bool Prefix( )
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch( typeof( Radio ), "QueueClip" )]
+    class Radio_QueueClipPatch
+    {
+        static bool Prefix( Radio.ClipInfo clip, bool pushToFront )
+        {
+            if ( clip.m_SegmentType != SegmentType.Playlist )
+            {
+                UnityEngine.Debug.Log( "Skipped radio clip: " + clip.m_SegmentType.ToString( ) );
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
